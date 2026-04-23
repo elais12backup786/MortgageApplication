4f7c03c9cc983e843e68dc13b097a2fb12c5e8a3
elais12backup786 <elais12backup@gmail.com>
2026-04-23 17:27:14 +0530

    Add CI code display

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Ehtesham Lais !");
+        System.out.println("Hello, Ehtesham Lais - Test for CI !");
     }
 }
